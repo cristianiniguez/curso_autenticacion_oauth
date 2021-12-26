f20829a5315531d98c7ba0cff7d0bf3423c18b38
cristianiniguez <felizpc1@gmail.com>
2021-12-26 15:45:14 -0400

    oauth 2.0: implementando client credentials grant

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -4,6 +4,10 @@ dotenv.config();
 
 const config = {
   authJwtSecret: process.env.AUTH_JWT_SECRET as string,
+  spotify: {
+    clientId: process.env.SPOTIFY_CLIENT_ID as string,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+  },
 };
 
 export default config;
